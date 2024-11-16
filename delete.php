fe154754d7c901327ba3a6cd24053cf628392d0c
Pat-TX <williams.patrick.d@gmail.com>
2024-11-16 15:38:52 -0600

    added delete functionality

--- delete.php
+++ delete.php
@@ -38,30 +38,21 @@
 
     <br />
 
-    <form method="GET" action="delete.php">
+    <form method="POST" action="delete.php">
         <div class="row">
             <div class="col-sm-2">
-                <label for="name" class="col-form-label">Search by Item Name:</label>
+                <label for="name" class="col-form-label">Item Name to be Deleted:</label>
             </div>
             <div class="col-sm-10">
-                <input class="form-control" type="text" id="name" name="name" placeholder="Item Name"><br><br>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-2">
-                <label for="id" class="col-form-label">Search by Item ID:</label>
-            </div>
-            <div class="col-sm-10">
-                <input class="form-control" type="text" id="id" name="id" placeholder="Item ID"><br><br>
+                <input class="form-control" type="text" id="dname" name="dname" placeholder="Item Name"><br><br>
             </div>
         </div>
 
         <div class="row text-center">
             <div class="d-grid mb-3">
-            
-                <button type="submit" class="btn btn-primary">Search for Item</button>
-            
+
+                <button type="submit" class="btn btn-primary" name="submit">Delete Item</button>
+
             </div>
         </div>
 
@@ -73,42 +64,62 @@
 
     $sql = "SELECT iId, Iname, Sprice, Idescription FROM item";
 
-    $name = isset($_GET['name']) ? trim($_GET['name']) :'';
-    $id = isset($_GET['id']) ? trim($_GET['id']) :'';
-
-    if (!empty($name) && !empty($id)) 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) 
     {
-        echo "<div class='alert alert-danger' role='alert'>
-                Only one field should be used at a time! Showing all data.
+        $dname = trim($_POST['dname']);
+
+        if (empty($dname)) 
+        {
+            echo "<div class='alert alert-danger' role='alert'>
+                Please enter the item name to be deleted!
               </div>";
-    }
-    elseif(!empty($name) || !empty($id))
-    {
-        $sql = '';
-
-        if (!empty($name))
+        } 
+        else 
         {
-            $toSearch = mysqli_real_escape_string($connection, $name);
-            $sql = "SELECT iId, Iname, Sprice, Idescription FROM item WHERE Iname LIKE '%$toSearch%'";
-        }
-        else if (!empty($id))
-        {
-            $toSearch = mysqli_real_escape_string($connection, $id);
-            $sql = "SELECT iId, Iname, Sprice, Idescription FROM item WHERE iId = '$toSearch'";
+            $check_sql = "SELECT Iname FROM item WHERE Iname = ?";
+            $check_stmt = mysqli_prepare($connection, $check_sql);
+            mysqli_stmt_bind_param($check_stmt, "s", $dname);
+            mysqli_stmt_execute($check_stmt);
+            mysqli_stmt_store_result($check_stmt);
+
+            if (mysqli_stmt_num_rows($check_stmt) == 0) 
+            {
+                echo "<div class='alert alert-danger' role='alert'>
+                        Item '$dname' not found in the database. Please check the name and try again.
+                      </div>";
+            } 
+            else 
+            {
+                $sql = "DELETE FROM item WHERE Iname = ?";
+                $stmt = mysqli_prepare($connection, $sql);
+                mysqli_stmt_bind_param($stmt, "s", $dname);
+
+                if (mysqli_stmt_execute($stmt)) 
+                {
+                    echo "<div class='alert alert-success' role='alert'>Item deleted successfully!</div>";
+                } 
+                else 
+                {
+                    echo "<div class='alert alert-danger' role='alert'>Error: Could not delete data in the database.</div>";
+                }
+            }
         }
     }
 
+    $sql = "SELECT iId, Iname, Sprice, Idescription FROM item";
     // Query the database
     $result = mysqli_query($connection, $sql);
 
     // Check if there are results
-    if (mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($result) > 0) 
+    {
         // Display data in a table
         echo "<table class='table table-striped table-hover'>";
         echo "<tr><th scope='col'>Item ID</th><th scope='col'>Item Name</th><th scope='col'>Price</th><th scope='col'>Description</th></tr>";
 
         // Fetch and display each row of data
-        while ($row = mysqli_fetch_assoc($result)) {
+        while ($row = mysqli_fetch_assoc($result)) 
+        {
             echo "<tr>";
             echo "<td>" . htmlspecialchars($row['iId']) . "</td>";
             echo "<td>" . htmlspecialchars($row['Iname']) . "</td>";
@@ -118,7 +129,9 @@
         }
 
         echo "</table>";
-    } else {
+    } 
+    else 
+    {
         echo "<div class='alert alert-info' role='alert'>
                 No results found.
               </div>";
